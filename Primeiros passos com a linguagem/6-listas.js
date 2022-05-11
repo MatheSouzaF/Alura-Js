aa9c881bd2abfc4bee6bb1e7a969c1d777de3d73
MatheSouzaF <matheusouzafloriani@gmail.com>
2022-05-11 18:10:54 -0300

    Primeiras Aulas check_02!

---
+++ Primeiros passos com a linguagem/6-listas.js
@@ -0,0 +1,11 @@
+console.log(`Trabalhando com listas`);
+
+const listaDeDestinos = new Array(
+    `Gramado`,
+    `Urubici`,
+    `Gaspar`,
+);
+//Adicionando item no elemento!
+listaDeDestinos.push(`Blumenal`);
+
+console.log(listaDeDestinos);
