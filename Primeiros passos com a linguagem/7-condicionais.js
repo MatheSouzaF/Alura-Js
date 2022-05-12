3b5a33e568ee1af5cdb6c68cfb48becc02fe9f9e
MatheSouzaF <matheusouzafloriani@gmail.com>
2022-05-12 10:27:00 -0300

    Primeiros passos JS_03

---
+++ Primeiros passos com a linguagem/7-condicionais.js
@@ -0,0 +1,16 @@
+console.log(`Trabalhando com condicionais`);
+
+const listaDeDestinos = new Array(
+    `Gramado`,
+    `Urubici`,
+    `Gaspar`,
+);
+
+const idadeComprador = 15;
+console.log("Destinos possíveis: ");
+console.log(listaDeDestinos);
+
+//Removendo item no elemento!
+listaDeDestinos.splice(3,1);
+console.log(listaDeDestinos);
+
