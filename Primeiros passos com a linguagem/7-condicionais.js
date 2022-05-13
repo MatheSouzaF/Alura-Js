18710df09242ad4e2b78c297141fa9c26ac7e5d5
Matheus de souza <matheusouzafloriani@gmail.com>
2022-05-12 21:47:14 -0300

    Update 7-condicionais.js

--- Primeiros passos com a linguagem/7-condicionais.js
+++ Primeiros passos com a linguagem/7-condicionais.js
@@ -6,3 +6,47 @@ const listaDeDestinos = new Array(
     `Gaspar`,
 );
 
+
+<<<<<<< aula
+const idadeComprador = 18;
+const estaAcompanhada = false
+const temPassagemComprada = true
+
+console.log("Destinos possíveis: ");
+console.log(listaDeDestinos);
+
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(2, 1);//Removendo item no elemento!
+// } else if (estaAcompanhada) {
+//     console.log("Comprador está acompanhado");
+//     listaDeDestinos.splice(2, 1);//Removendo item no elemento!
+
+// } else {
+//     console.log("Não  é maior de Idade e não posso vender");
+// }
+
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa Viagem!");
+    listaDeDestinos.splice(2, 1);//Removendo item no elemento!
+} else {
+    console.log("Não  é maior de Idade e não posso vender");
+}
+
+console.log("Embarque: \n\n");
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa Viagem!")
+}else{
+    console.log("Voce não pode embarcar")
+}
+
+=======
+const idadeComprador = 15;
+console.log("Destinos possíveis: ");
+console.log(listaDeDestinos);
+
+//Removendo item no elemento!
+listaDeDestinos.splice(3,1);
+>>>>>>> master
+console.log(listaDeDestinos);
